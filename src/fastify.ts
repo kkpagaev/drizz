c8b3d5b7fe1c41835e592d3081c9f7b3d6dbd9fc
kkpagaev <kkpagaev@gmail.com>
2023-12-17 00:19:35 +0200

    moved fastify to file

---
+++ src/fastify.ts
@@ -0,0 +1,47 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import Fastify from "fastify"
+import { ZodSchema } from "zod"
+import api from "./api"
+
+export async function buildFastify() {
+  const f = Fastify({
+    logger: true
+  })
+
+  f.setValidatorCompiler<ZodSchema>((param) => {
+    return (data) => {
+      const result = param.schema.safeParse(data)
+      if (result.success === true) {
+        return {
+          value: result.data
+        }
+      } else {
+        return {
+          error: result.error
+        }
+      }
+    }
+  }).setSerializerCompiler(({ schema }: { schema: ZodSchema }) => {
+    return (data) => {
+      const result = schema.safeParse(data)
+      if (result.success) {
+        return JSON.stringify(result.data)
+      }
+
+      return JSON.stringify({
+        error: result.error
+      })
+    }
+  })
+  f.setErrorHandler<Error>(async (error, request, reply) => {
+    f.log.error(error)
+    return reply.status(400).send({
+      status: 400,
+      message: error.message
+    })
+  })
+
+  await f.register(api)
+
+  return f.withTypeProvider<ZodTypeProvider>()
+}
--- src/index.ts
+++ src/index.ts
@@ -2,63 +2,17 @@ import "./glob"
 import * as dotenv from "dotenv"
 import { InferSelectModel } from "drizzle-orm"
 import { users } from "./schema"
-import Fastify from "fastify"
-import { ZodTypeProvider } from "fastify-type-provider-zod"
 import { client } from "./db"
-import { ZodSchema } from "zod"
-import api from "./api"
+import { buildFastify } from "./fastify"
 dotenv.config()
 
 export type User = InferSelectModel<typeof users>
 
-async function build() {
-  const f = Fastify({
-    logger: true
-  })
-
-  f.setValidatorCompiler<ZodSchema>((param) => {
-    return (data) => {
-      const result = param.schema.safeParse(data)
-      if (result.success === true) {
-        return {
-          value: result.data
-        }
-      } else {
-        return {
-          error: result.error
-        }
-      }
-    }
-  }).setSerializerCompiler(({ schema }: { schema: ZodSchema }) => {
-    return (data) => {
-      const result = schema.safeParse(data)
-      if (result.success) {
-        return JSON.stringify(result.data)
-      }
-
-      return JSON.stringify({
-        error: result.error
-      })
-    }
-  })
-  f.setErrorHandler<Error>(async (error, request, reply) => {
-    f.log.error(error)
-    return reply.status(400).send({
-      status: 400,
-      message: error.message
-    })
-  })
-
-  return f.withTypeProvider<ZodTypeProvider>()
-}
-
-export type FastifyApp = Awaited<ReturnType<typeof build>>
+export type FastifyApp = Awaited<ReturnType<typeof buildFastify>>
 
 async function main() {
   await client.connect()
-  const f = await build()
-
-  await f.register(api)
+  const f = await buildFastify()
 
   f.listen({ port: 3000 }, (err, address) => {
     if (err) {
