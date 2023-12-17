1f110fcd0715c8f77ceff5cf469645f621551692
kkpagaev <kkpagaev@gmail.com>
2023-12-17 07:23:25 +0200

    moved to secrete to env

--- src/services/jwt.ts
+++ src/services/jwt.ts
@@ -1,12 +1,14 @@
 import * as jwt from "jsonwebtoken"
 
+const secret = process.env.JWT_SECRET || "secret"
+
 export interface JwtPayload extends jwt.JwtPayload {
   userId: number
 }
 export function generateToken(payload: JwtPayload): string {
-  return jwt.sign(payload, "secret")
+  return jwt.sign(payload, secret)
 }
 
 export function verifyToken(token: string): JwtPayload {
-  return jwt.verify(token, "secret") as JwtPayload
+  return jwt.verify(token, secret) as JwtPayload
 }
